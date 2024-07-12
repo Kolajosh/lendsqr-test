68977d9c4bdad27c80629266b94ab7331fa20249
Kolawole <kolawole.joshua@wemabank.com>
2024-07-12 15:45:30 +0100

    Update: Refactored some files to TS, and removed some icons that were not in use

---
+++ src/utils/icons.tsx
@@ -0,0 +1,248 @@
+import * as React from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
+
+export function IconSearch(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 512 512" {...props}>
+      <path
+        d="M443.5 420.2L336.7 312.4c20.9-26.2 33.5-59.4 33.5-95.5 0-84.5-68.5-153-153.1-153S64 132.5 64 217s68.5 153 153.1 153c36.6 0 70.1-12.8 96.5-34.2l106.1 107.1c3.2 3.4 7.6 5.1 11.9 5.1 4.1 0 8.2-1.5 11.3-4.5 6.6-6.3 6.8-16.7.6-23.3zm-226.4-83.1c-32.1 0-62.3-12.5-85-35.2-22.7-22.7-35.2-52.9-35.2-84.9 0-32.1 12.5-62.3 35.2-84.9 22.7-22.7 52.9-35.2 85-35.2s62.3 12.5 85 35.2c22.7 22.7 35.2 52.9 35.2 84.9 0 32.1-12.5 62.3-35.2 84.9-22.7 22.7-52.9 35.2-85 35.2z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconNotification(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 20 20" {...props}>
+      <path
+        fill="currentColor"
+        d="M9.985 0c1.089 0 1.971.898 1.971 2.006l-.009.163c.868.352 1.707.936 2.451 1.71c.862.893 1.366 2.077 1.521 3.596v5.478l1.191 2.098c.4.666.528 1.224.216 1.707c-.286.441-.797.595-1.49.583h-2.67C12.854 18.86 11.532 20 9.95 20c-1.584 0-2.905-1.14-3.216-2.658H3.778l-.056-.003c-.627-.054-1.094-.357-1.199-.94c-.071-.397.023-.823.268-1.331l1.225-2.18l.003-5.473c.107-1.21.56-2.337 1.348-3.371c.667-.875 1.62-1.519 2.654-1.89a1.752 1.752 0 0 1-.006-.148C8.015.898 8.897 0 9.985 0Zm1.818 17.342H8.097c.275.77 1 1.32 1.853 1.32c.852 0 1.578-.55 1.853-1.32ZM10.082 3.124c-1.354 0-2.843.645-3.677 1.74c-.638.836-.994 1.722-1.075 2.61v5.59c0 .117-.03.232-.087.333l-1.291 2.296a1.71 1.71 0 0 0-.12.311h12.014c.121.002.213-.003.276-.005a2.615 2.615 0 0 0-.141-.265l-1.287-2.267a.678.678 0 0 1-.088-.335l.003-5.586c-.121-1.162-.506-2.064-1.149-2.732c-1.04-1.08-2.262-1.69-3.378-1.69Zm-.097-1.787a.66.66 0 0 0-.635.497c.246-.031.49-.047.732-.047c.177 0 .356.01.535.032a.66.66 0 0 0-.632-.482Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconCaretDownOutlined(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 1024 1024" {...props}>
+      <path
+        fill="currentColor"
+        d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconBriefcase(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 512 512" {...props}>
+      <path
+        fill="currentColor"
+        d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconAngleDown(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M17 9.17a1 1 0 0 0-1.41 0L12 12.71L8.46 9.17a1 1 0 0 0-1.41 0a1 1 0 0 0 0 1.42l4.24 4.24a1 1 0 0 0 1.42 0L17 10.59a1 1 0 0 0 0-1.42Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconHome(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 512 512" {...props}>
+      <path
+        fill="currentColor"
+        d="M261.56 101.28a8 8 0 0 0-11.06 0L66.4 277.15a8 8 0 0 0-2.47 5.79L63.9 448a32 32 0 0 0 32 32H192a16 16 0 0 0 16-16V328a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8v136a16 16 0 0 0 16 16h96.06a32 32 0 0 0 32-32V282.94a8 8 0 0 0-2.47-5.79Z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="m490.91 244.15l-74.8-71.56V64a16 16 0 0 0-16-16h-48a16 16 0 0 0-16 16v32l-57.92-55.38C272.77 35.14 264.71 32 256 32c-8.68 0-16.72 3.14-22.14 8.63l-212.7 203.5c-6.22 6-7 15.87-1.34 22.37A16 16 0 0 0 43 267.56L250.5 69.28a8 8 0 0 1 11.06 0l207.52 198.28a16 16 0 0 0 22.59-.44c6.14-6.36 5.63-16.86-.76-22.97Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconUserCheck(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        d="M8 12.052c1.995 0 3.5-1.505 3.5-3.5s-1.505-3.5-3.5-3.5s-3.5 1.505-3.5 3.5s1.505 3.5 3.5 3.5zM9 13H7c-2.757 0-5 2.243-5 5v1h12v-1c0-2.757-2.243-5-5-5zm11.294-4.708l-4.3 4.292l-1.292-1.292l-1.414 1.414l2.706 2.704l5.712-5.702z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconUserX(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        d="M8 12.052c1.995 0 3.5-1.505 3.5-3.5s-1.505-3.5-3.5-3.5s-3.5 1.505-3.5 3.5s1.505 3.5 3.5 3.5zM9 13H7c-2.757 0-5 2.243-5 5v1h12v-1c0-2.757-2.243-5-5-5zm11.293-4.707L18 10.586l-2.293-2.293l-1.414 1.414l2.292 2.292l-2.293 2.293l1.414 1.414l2.293-2.293l2.294 2.294l1.414-1.414L19.414 12l2.293-2.293z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconFilterList(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M11 18h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm4 6h10c.55 0 1-.45 1-1s-.45-1-1-1H7c-.55 0-1 .45-1 1s.45 1 1 1z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconMenuKebab(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 16 16" {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+        <circle cx="8" cy="2.5" r=".75"></circle>
+        <circle cx="8" cy="8" r=".75"></circle>
+        <circle cx="8" cy="13.5" r=".75"></circle>
+      </g>
+    </svg>
+  );
+}
+
+export function IconEyeOutline(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 15 15" {...props}>
+      <path
+        fill="currentColor"
+        d="m.5 7.5l-.464-.186a.5.5 0 0 0 0 .372L.5 7.5Zm14 0l.464.186a.5.5 0 0 0 0-.372L14.5 7.5Zm-7 4.5c-2.314 0-3.939-1.152-5.003-2.334a9.368 9.368 0 0 1-1.449-2.164a5.065 5.065 0 0 1-.08-.18l-.004-.007v-.001L.5 7.5l-.464.186v.002l.003.004a2.107 2.107 0 0 0 .026.063l.078.173a10.368 10.368 0 0 0 1.61 2.406C2.94 11.652 4.814 13 7.5 13v-1Zm-7-4.5l.464.186l.004-.008a2.62 2.62 0 0 1 .08-.18a9.368 9.368 0 0 1 1.449-2.164C3.56 4.152 5.186 3 7.5 3V2C4.814 2 2.939 3.348 1.753 4.666a10.367 10.367 0 0 0-1.61 2.406a6.05 6.05 0 0 0-.104.236l-.002.004v.001H.035L.5 7.5Zm7-4.5c2.314 0 3.939 1.152 5.003 2.334a9.37 9.37 0 0 1 1.449 2.164a4.705 4.705 0 0 1 .08.18l.004.007v.001L14.5 7.5l.464-.186v-.002l-.003-.004a.656.656 0 0 0-.026-.063a9.094 9.094 0 0 0-.39-.773a10.365 10.365 0 0 0-1.298-1.806C12.06 3.348 10.186 2 7.5 2v1Zm7 4.5a68.887 68.887 0 0 1-.464-.186l-.003.008l-.015.035l-.066.145a9.37 9.37 0 0 1-1.449 2.164C11.44 10.848 9.814 12 7.5 12v1c2.686 0 4.561-1.348 5.747-2.665a10.366 10.366 0 0 0 1.61-2.407a6.164 6.164 0 0 0 .104-.236l.002-.004v-.001h.001L14.5 7.5ZM7.5 9A1.5 1.5 0 0 1 6 7.5H5A2.5 2.5 0 0 0 7.5 10V9ZM9 7.5A1.5 1.5 0 0 1 7.5 9v1A2.5 2.5 0 0 0 10 7.5H9ZM7.5 6A1.5 1.5 0 0 1 9 7.5h1A2.5 2.5 0 0 0 7.5 5v1Zm0-1A2.5 2.5 0 0 0 5 7.5h1A1.5 1.5 0 0 1 7.5 6V5Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconAngleLeftB(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="m8.5 12.8l5.7 5.6c.4.4 1 .4 1.4 0c.4-.4.4-1 0-1.4l-4.9-5l4.9-5c.4-.4.4-1 0-1.4c-.2-.2-.4-.3-.7-.3c-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6c0-.1 0-.1 0 0z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconAngleRightB(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M15.54 11.29L9.88 5.64a1 1 0 0 0-1.42 0a1 1 0 0 0 0 1.41l4.95 5L8.46 17a1 1 0 0 0 0 1.41a1 1 0 0 0 .71.3a1 1 0 0 0 .71-.3l5.66-5.65a1 1 0 0 0 0-1.47Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconLongLeft(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path fill="currentColor" d="M6 16v-3h16v-2H6V8l-4 4l4 4Z"></path>
+    </svg>
+  );
+}
+
+export function IconStarPurple500(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2L9.19 8.63L2 9.24l5.46 4.73L5.82 21L12 17.27z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconSidebarExpandLeft(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 48 48" {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      >
+        <path d="M6 9a3 3 0 0 1 3-3h30a3 3 0 0 1 3 3v30a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3V9Z"></path>
+        <path
+          strokeLinecap="round"
+          d="M32 6v36M16 20l4 4l-4 4M26 6h12M26 42h12"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export function IconSidebarExpandRight(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 48 48" {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      >
+        <rect width="36" height="36" x="6" y="6" rx="3"></rect>
+        <path
+          strokeLinecap="round"
+          d="M18 6v36M11 6h25M11 42h25m-4-22l-4 4l4 4"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export function IconHambuger(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M201.002 111c4.417 0 7.998 3.579 7.998 8.01v16.98c0 4.424-3.588 8.01-7.998 8.01H55.998C51.58 144 48 140.421 48 135.99v-16.98c0-4.424 3.588-8.01 7.998-8.01h145.004zm0 65c4.417 0 7.998 3.579 7.998 8.01v16.98c0 4.424-3.588 8.01-7.998 8.01H55.998C51.58 209 48 205.421 48 200.99v-16.98c0-4.424 3.588-8.01 7.998-8.01h145.004zm0-129c4.417 0 7.998 3.579 7.998 8.01v16.98c0 4.424-3.588 8.01-7.998 8.01H55.998C51.58 80 48 76.421 48 71.99V55.01c0-4.424 3.588-8.01 7.998-8.01h145.004z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconDropDown(props: IconProps) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 20 20" {...props}>
+      <path
+        fill="currentColor"
+        d="m17.5 4.75l-7.5 7.5l-7.5-7.5L1 6.25l9 9l9-9z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IconSpinner(props: IconProps) {
+  return (
+    <svg width="1.05em" height="1em" viewBox="0 0 25 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M4.818 6.664h-.001a1.847 1.847 0 1 1 1.306-.541a1.773 1.773 0 0 1-1.277.541h-.029zm-2.97 7.182h-.001a1.847 1.847 0 1 1 1.306-.541a1.773 1.773 0 0 1-1.278.541h-.031h.002zM12 3.692h-.001a1.847 1.847 0 1 1 1.306-.541a1.773 1.773 0 0 1-1.277.541h-.029zM4.818 21.029h-.001a1.847 1.847 0 1 1 1.306-.541a1.769 1.769 0 0 1-1.276.541h-.031zM19.182 7.125a2.308 2.308 0 1 1 0-4.615a2.308 2.308 0 0 1 0 4.615zM12 24h-.001a1.847 1.847 0 1 1 1.306-.541a1.773 1.773 0 0 1-1.277.541h-.029zm10.154-9.231h-.048c-.75 0-1.428-.309-1.913-.807l-.001-.001c-.499-.503-.808-1.196-.808-1.961s.308-1.458.808-1.962a2.663 2.663 0 0 1 1.914-.808h.05h-.003h.048c.75 0 1.427.309 1.913.807l.001.001c.499.503.808 1.196.808 1.961s-.308 1.458-.808 1.962a2.664 2.664 0 0 1-1.915.809h-.049h.002zm-2.971 7.643h-.05a3.097 3.097 0 0 1-2.236-.951l-.001-.001c-.584-.584-.945-1.391-.945-2.283s.361-1.698.945-2.283a3.106 3.106 0 0 1 2.234-.945h.054h-.003h.042c.877 0 1.67.362 2.237.944l.001.001c.588.582.952 1.39.952 2.283s-.364 1.7-.952 2.282a3.102 3.102 0 0 1-2.24.953h-.04z"
+      ></path>
+    </svg>
+  );
+}
